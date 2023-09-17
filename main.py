b7c75fd903350aa0e9537f38d06ffedf1239abe6
xxcosita3cz <kokosove18@gmail.com>
2023-09-17 09:40:38 +0200

    codin

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import customtkinter as ctk
 
-class Main(ctk.CTk()):
-	def __init__(self):
-		super().__init__()
+class App(ctk.CTk):
+    def __init__(self):
+    	# INIT of Self
+        super().__init__()
+
+		# Window config
+        self.title("servmanGUI")
+        self.geometry(f"{600}x{400}")
+        self.resizable(False,False)
+
+		# Frames
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
